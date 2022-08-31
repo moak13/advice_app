d1767ed350c5148adc7905175c2cfce4d7f5ca15
moak13 <michaelkalango@gmail.com>
2022-08-31 23:38:58 +0100

    feat: initial splash view

---
+++ lib/features/splash/splash_view.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
+
+import 'view_model/splash_viewmodel.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ViewModelBuilder<SplashViewModel>.reactive(
+      viewModelBuilder: () => SplashViewModel(),
+      builder: (
+        BuildContext context,
+        SplashViewModel model,
+        Widget? child,
+      ) {
+        return const Scaffold(
+          body: Center(
+            child: Text(
+              'Splash View',
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
