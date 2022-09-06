75999c01f577273004be903cd7f7cd074e72bb7f
moak13 <michaelkalango@gmail.com>
2022-09-06 20:19:06 +0100

    chore: advice

--- lib/features/advice/advice_view.dart
+++ lib/features/advice/advice_view.dart
@@ -1,7 +1,11 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 
 import '../../core/models/slip.dart';
+import '../../core/utils/size_manager.dart';
+import '../../core/shared_widgets/info_card.dart';
 import 'view_model/advice_viewmodel.dart';
 
 class AdviceView extends StatelessWidget {
@@ -13,6 +17,8 @@ class AdviceView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    SizeMg.init(context);
     return ViewModelBuilder<AdviceViewModel>.reactive(
       viewModelBuilder: () => AdviceViewModel(),
       builder: (
@@ -20,10 +26,65 @@ class AdviceView extends StatelessWidget {
         AdviceViewModel model,
         Widget? child,
       ) {
-        return const Scaffold(
+        return Scaffold(
+          backgroundColor: theme.scaffoldBackgroundColor,
+          appBar: AppBar(
+            backgroundColor: theme.appBarTheme.backgroundColor,
+            elevation: 0,
+          ),
           body: Center(
-            child: Text(
-              'Advice View',
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const InfoCard(
+                  informationText:
+                      'Make use of the icon below to save an advice that speaks most to you.',
+                ),
+                SizedBox(
+                  height: SizeMg.height(100),
+                ),
+                Container(
+                  height: SizeMg.height(100),
+                  width: SizeMg.width(100),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors
+                        .primaries[Random().nextInt(Colors.primaries.length)],
+                  ),
+                  child: FittedBox(
+                    child: Text(
+                      '${slip?.id}',
+                      style: theme.textTheme.displaySmall?.copyWith(
+                        fontSize: SizeMg.text(40),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: SizeMg.height(30),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                    left: SizeMg.width(20),
+                    right: SizeMg.width(20),
+                  ),
+                  child: Text(
+                    '${slip?.advice}',
+                    textAlign: TextAlign.center,
+                    style: theme.textTheme.bodyLarge,
+                  ),
+                ),
+                SizedBox(
+                  height: SizeMg.height(30),
+                ),
+                IconButton(
+                  onPressed: () {
+                    model.actionSaveAdvice(slip: slip);
+                  },
+                  icon: const Icon(Icons.save),
+                ),
+              ],
             ),
           ),
         );
--- lib/features/advice/view_model/advice_viewmodel.dart
+++ lib/features/advice/view_model/advice_viewmodel.dart
@@ -1,3 +1,40 @@
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
 
-class AdviceViewModel extends BaseViewModel {}
+import '../../../core/app/app.locator.dart';
+import '../../../core/enum/snackbar_enum.dart';
+import '../../../core/models/slip.dart';
+import '../../../core/stores/slip_store.dart';
+
+class AdviceViewModel extends BaseViewModel {
+  final _snackbarService = locator<SnackbarService>();
+  final _slipStore = locator<SlipStore>();
+
+  void actionSaveAdvice({Slip? slip}) async {
+    if (await _slipStore.isSaved(slipId: slip?.id)) {
+      return _handleSnackbar(
+        title: 'Error',
+        message: 'An advice with the ID: ${slip?.id} has already been saved.',
+        variant: SnackBarType.error,
+      );
+    }
+    await _slipStore.addAdvice(slip: slip);
+    return _handleSnackbar(
+      title: 'Info',
+      message: 'An advice with an ID: ${slip?.id} has been saved.',
+      variant: SnackBarType.info,
+    );
+  }
+
+  void _handleSnackbar({
+    String? title,
+    required String message,
+    dynamic variant,
+  }) {
+    _snackbarService.showCustomSnackBar(
+      title: title,
+      message: message,
+      variant: variant,
+    );
+  }
+}
