d59f2917d994df64dd1fe977a0cbd052a228177f
moak13 <michaelkalango@gmail.com>
2022-09-07 01:02:30 +0100

    chore: advices implementation

--- lib/features/advices/view_model/advices_viewmodel.dart
+++ lib/features/advices/view_model/advices_viewmodel.dart
@@ -1,3 +1,29 @@
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
 
-class AdvicesViewModel extends BaseViewModel {}
+import '../../../core/app/app.locator.dart';
+import '../../../core/app/app.logger.dart';
+import '../../../core/enum/snackbar_enum.dart';
+import '../../../core/models/slip.dart';
+import '../../../core/stores/slip_store.dart';
+
+class AdvicesViewModel extends StreamViewModel<List<Slip>> {
+  final _slipStore = locator<SlipStore>();
+  final _snackbarService = locator<SnackbarService>();
+  final _log = getLogger('AdvicesViewModel');
+
+  void actionDeleteAdvice({Slip? slip}) async {
+    _log.i('deleting advice');
+    await _slipStore.removeAdvice(slip: slip);
+    _snackbarService.showCustomSnackBar(
+      title: 'Info',
+      message: 'An advice with the ID: ${slip?.id} has been deleted.',
+      variant: SnackBarType.info,
+    );
+    // This helps to refresh the ViewModel once the contract is called.
+    initialise();
+  }
+
+  @override
+  Stream<List<Slip>> get stream => _slipStore.getStreamAdvice();
+}
--- lib/features/home/widgets/contents.dart
+++ lib/features/home/widgets/contents.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 
+import '../../../core/shared_widgets/info_card.dart';
 import '../../../core/utils/size_manager.dart';
 import '../view_model/home_viewmodel.dart';
 import 'action_button.dart';
-import '../../../core/shared_widgets/info_card.dart';
 
 class Contents extends ViewModelWidget<HomeViewModel> {
   const Contents({Key? key}) : super(key: key, reactive: false);
